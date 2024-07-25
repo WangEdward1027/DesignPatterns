9bee66e9c45b0c6c4ed4506a53078f11a45f19c3
Edward <78650387+WangEdward1027@users.noreply.github.com>
2024-07-25 21:20:46 +0800

    工厂方法模式：抽象类工厂，下面每一个派生类工厂，只生产对应的一种产品。 每要新增一种产品,就要新增一个产品工厂。

---
+++ Factory/factoryMethod.cpp
@@ -0,0 +1,197 @@
+//工厂方法模式:抽象类工厂，下面每一个派生类工厂，只生产对应的一种产品。
+//每要新增一种产品,就要新增一个产品工厂。
+
+#include <math.h>
+#include <iostream>
+#include <string>
+#include <memory>
+
+using std::cout;
+using std::endl;
+using std::string;
+using std::unique_ptr;
+
+class Figure
+{
+public:
+    virtual void display() const = 0;
+    virtual double area() const = 0;
+    virtual ~Figure(){}
+};
+
+class Rectangle
+: public Figure
+{
+public:
+    Rectangle(double length = 0, double width = 0)
+    : _length(length)
+    , _width(width)
+    {
+        cout << "Rectangle(double = 0, double = 0)" << endl;
+    }
+
+    void display() const override
+    {
+        cout << "Rectangle";
+    }
+
+    double area() const override
+    {
+        return _length * _width;
+    }
+
+    ~Rectangle()
+    {
+        cout << "~Rectangle()" << endl;
+    }
+private:
+    double _length;
+    double _width;
+};
+
+class Circle
+: public Figure
+{
+public:
+    Circle(double radius = 0)
+    : _radius(radius)
+    {
+        cout << "Circle(double = 0)" << endl;
+    }
+
+    void display() const override
+    {
+        cout << "Circle";
+    }
+
+    double area() const override
+    {
+        return 3.14 * _radius *_radius;;
+    }
+
+    ~Circle()
+    {
+        cout << "~Circle()" << endl;
+    }
+private:
+    double _radius;
+};
+
+class Triangle
+: public Figure
+{
+public:
+    Triangle(double a = 0, double b = 0, double c = 0)
+    : _a(a)
+    , _b(b)
+    , _c(c)
+    {
+        cout << "Triangle(double = 0, double = 0, double = 0)" << endl;
+    }
+
+    void display() const override
+    {
+        cout << "Triangle";
+    }
+
+    double area() const override
+    {
+        //海伦公式
+        double tmp = (_a + _b + _c)/2;
+
+        return sqrt(tmp * (tmp - _a) * (tmp - _b) * (tmp - _c));
+    }
+
+    ~Triangle()
+    {
+        cout << "~Triangle()" << endl;
+    }
+private:
+    double _a;
+    double _b;
+    double _c;
+};
+
+void func(Figure *pfig)
+{
+    pfig->display();
+    cout << "的面积 : " << pfig->area() << endl;
+}
+
+//工厂方法
+//缺点：工厂的数量会随着产品数量的增加，而发生急剧的变化
+//
+//如果生产新的产品的时候，该模型的扩展性要好一些
+//优点：(如果从设计原则来看)
+//1、满足了单一职责原则
+//2、满足了开放闭合原则
+//3、满足了依赖导致原则
+class Factory
+{
+public:
+    virtual Figure *create() = 0;
+    virtual ~Factory(){}
+};
+
+class RectangleFactory
+: public Factory
+{
+public:
+    virtual Figure *create() override
+    {
+        //可以通过配置文件进行获取
+        //配置文件的形式.txt、xml、bat、yang
+        //读取配置文件，然后从配置文件中获取所需的数据
+        Rectangle *prec = new Rectangle(10, 20);
+
+        return prec;;
+    }
+};
+
+class CircleFactory
+: public Factory
+{
+public:
+    virtual Figure *create() override
+    {
+        //可以通过配置文件进行获取
+        //配置文件的形式.txt、xml、bat、yang
+        //读取配置文件，然后从配置文件中获取所需的数据
+        Circle *pcir = new Circle(10);
+
+        return pcir;;
+    }
+};
+
+class TriangleFactory
+: public Factory
+{
+public:
+    virtual Figure *create() override
+    {
+        //可以通过配置文件进行获取
+        //配置文件的形式.txt、xml、bat、yang
+        //读取配置文件，然后从配置文件中获取所需的数据
+        Triangle *ptri = new Triangle(3, 4, 5);
+
+        return ptri;
+    }
+};
+
+int main(int argc, char **argv)
+{
+    unique_ptr<Factory> precFac(new RectangleFactory());
+    unique_ptr<Figure> prec(precFac->create());
+
+    unique_ptr<Factory> pcirFac(new CircleFactory());
+    unique_ptr<Figure> pcir(pcirFac->create());
+
+    unique_ptr<Factory> ptriFac(new TriangleFactory());
+    unique_ptr<Figure> ptri(ptriFac->create());
+
+    func(prec.get());
+    func(pcir.get());
+    func(ptri.get());
+
+    return 0;
+}
