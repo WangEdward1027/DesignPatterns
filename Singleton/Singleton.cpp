1d267ec59e714a3ba35f61f69d9a2c9065881b6f
Edward <78650387+WangEdward1027@users.noreply.github.com>
2024-09-23 21:59:50 +0800

    单例模式：懒加载

--- Singleton/Singleton.cpp
+++ Singleton/Singleton.cpp
@@ -1,60 +1,35 @@
-//单例模式:懒加载
-
 #include <iostream> 
 using std::cout;
 using std::endl;
 
-//单例模式:要通过给的接口来创造唯一实例。
-//因此，要将构造函数私有化，并且删除拷贝构造和赋值运算符函数
-
 class Singleton
 {
 public:
-    //静态方法,用于获取唯一的实例。静态方法属于类，而非实例对象
-    //懒加载、懒汉式、延迟实例化
     static Singleton* getInstance()
     {
         if(pInstance == nullptr){
-            pInstance = new Singleton(); 
+            pInstance = new Singleton();
         }
         return pInstance;
     }
 
-    void display()
-    {
-        cout << "This is the Singleton instance." << "\n";
-    }
 private:
-    //构造函数私有化，防止外部创建对象
-    Singleton()
-    {
-        cout << "Singleton instance created.\n";
-    }
-
-    //禁用拷贝构造和赋值运算符函数
-    Singleton(const Singleton & ) = delete;
-    Singleton & operator=(const Singleton &) = delete;
-
-    //静态成员变量，存储唯一的实例
+    Singleton(){};
+    Singleton(const Singleton & rhs) = delete;
+    Singleton & operator= (const Singleton & rhs) = delete;
     static Singleton* pInstance;
 };
 
-//类外初始化静态成员变量
 Singleton* Singleton::pInstance = nullptr;
 
 int main()
 {
-    //获取单例对象的唯一实例
     Singleton* s1 = Singleton::getInstance();
-    s1->display();
-
-    //再次获取单例对象的实例
     Singleton* s2 = Singleton::getInstance();
-    s2->display();
-
-    //验证两个指针是否指向同一个实例
     if(s1 == s2){
-        cout << "Both pointer point to the same instance.\n";
+        cout << "s1 == s2,是单例的\n";
+    }else{
+        cout << "s1 != s2,不是单例的\n";
     }
 
     return 0;
